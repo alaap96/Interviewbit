8429b58a26db3bf39330abdc876090fa4d36ebe9
Vasireddy Alaap <alaap96@gmail.com>
2017-11-25 13:55:38 -0600

    Update Palindrome Integer.java

--- Programming/Math/Palindrome Integer.java
+++ Programming/Math/Palindrome Integer.java
@@ -12,3 +12,21 @@ public class Solution {
 	    return true;
 	}
 }
+// if extra space is not to be used then check with reverse 
+public class Solution {
+	public boolean isPalindrome(int a) {
+	    // check if reverse if the integer is same 
+	  if(a == reverse(a)){
+	      return true;
+	  }else return false;
+    }
+    // function to check if reverse is also same
+    public static int reverse(int a){
+        int num = 0;
+        while(a > 0){
+            num = num*10 + a % 10;
+            a = a/10;
+        }
+        return num;
+    } 
+}
