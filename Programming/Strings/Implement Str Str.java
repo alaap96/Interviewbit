18487a64ea3bacae42a974080450bdd10aee8da1
Vasireddy Alaap <alaap96@gmail.com>
2018-02-03 07:42:02 -0600

    Update Implement Str Str.java

--- Programming/Strings/Implement Str Str.java
+++ Programming/Strings/Implement Str Str.java
@@ -18,3 +18,19 @@ public class Solution {
 	    return -1;
 	}
 }
+// Another method
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public int strStr(final String A, final String B) {
+        if(A.length() == 0 || B.length() == 0) return -1;
+        for(int i = 0; i <= A.length() - B.length(); i++){
+            char current = A.charAt(i);
+            char first = B.charAt(0);
+            if(current == first){
+                if(A.substring(i,i+B.length()).equals(B)){return i;}
+            }
+        }
+        return -1;
+    }
+}
+
